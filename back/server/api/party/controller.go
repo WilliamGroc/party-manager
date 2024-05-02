cf6b791ea2c0811548435ccccd169e0cd83a3c62
William Groc <william.groc@sogeti.com>
2024-05-02 14:18:39 +0200

    Implement api for party

--- back/server/api/party/controller.go
+++ back/server/api/party/controller.go
@@ -8,6 +8,8 @@ import (
 	"partymanager/server/models"
 	"strconv"
 	"time"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func (ur *PartyRoutes) GetAllParty(w http.ResponseWriter, r *http.Request) {
@@ -24,9 +26,15 @@ func (ur *PartyRoutes) GetAllParty(w http.ResponseWriter, r *http.Request) {
 
 	var parties []models.Party
 
-	ur.DB.Find(&parties, "host_id = ?", idInt)
+	ur.DB.Not("deleted_at IS NOT NULL").Where("host_id = ?", idInt).Find(&parties)
 
-	
+	var response []PartyResponse
+
+	for _, party := range parties {
+		response = append(response, PartyResponse{ID: party.ID, Name: party.Name, Description: party.Description, Location: party.Location, Date: party.Date.String(), HostID: party.HostID})
+	}
+
+	api.EncodeBody(w, response)
 }
 
 func (ur *PartyRoutes) CreateParty(w http.ResponseWriter, r *http.Request) {
@@ -66,13 +74,98 @@ func (ur *PartyRoutes) CreateParty(w http.ResponseWriter, r *http.Request) {
 }
 
 func (ur *PartyRoutes) GetParty(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Get party"))
+	id := chi.URLParam(r, "id")
+
+	var party models.Party
+
+	ur.DB.First(&party, id)
+
+	api.EncodeBody(w, PartyResponse{ID: party.ID, Name: party.Name, Description: party.Description, Location: party.Location, Date: party.Date.String(), HostID: party.HostID})
 }
 
 func (ur *PartyRoutes) UpdateParty(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Update party"))
+	token, err := auth.GetToken(&r.Header, ur.Auth.TokenAuth)
+
+	if err != nil {
+		w.WriteHeader(http.StatusUnauthorized)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	userid, _ := token.Get("id")
+	idInt, _ := strconv.Atoi(fmt.Sprintf("%v", userid)) // Convert id to integer
+
+	id := chi.URLParam(r, "id")
+	partyId, _ := strconv.Atoi(id)
+
+	var party models.Party
+
+	ur.DB.Where(map[string]interface{}{"HostID": idInt, "ID": partyId}).First(&party)
+
+	if party.ID == 0 {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("Party not found"))
+		return
+	}
+
+	var body UpdatePartyRequest
+	api.DecodeBody(r, &body)
+
+	date, err := time.Parse("2006-01-02 03:04:05 -0700", body.Date)
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	if body.Name != "" {
+		party.Name = body.Name
+	}
+	if body.Description != "" {
+		party.Description = body.Description
+	}
+	if body.Location != "" {
+		party.Location = body.Location
+	}
+	if body.Date != "" {
+		party.Date = date
+	}
+
+	ur.DB.Save(&party)
+
+	api.EncodeBody(w, PartyResponse{ID: party.ID, Name: party.Name, Description: party.Description, Location: party.Location, Date: party.Date.String(), HostID: party.HostID})
 }
 
 func (ur *PartyRoutes) DeleteParty(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Delete party"))
+	token, err := auth.GetToken(&r.Header, ur.Auth.TokenAuth)
+
+	if err != nil {
+		w.WriteHeader(http.StatusUnauthorized)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	userid, _ := token.Get("id")
+	idInt, _ := strconv.Atoi(fmt.Sprintf("%v", userid)) // Convert id to integer
+
+	id := chi.URLParam(r, "id")
+	partyId, _ := strconv.Atoi(id)
+
+	var party models.Party
+	party.ID = uint(partyId)
+	party.HostID = idInt
+
+	ur.DB.First(&party)
+
+	if party.ID == 0 {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("Party not found"))
+		return
+	}
+
+	ur.DB.Model(&models.Party{}).Where("id  = ?", partyId).Update("deleted_at", time.Now())
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Party deleted"))
 }
